f7e0507322a37e97ba0bc20506dc38901247afff
William Hughes <whlabratz@gmail.com>
2016-09-19 22:08:10 +1200

    Converting to webpack

---
+++ assets/blackburn.js
@@ -0,0 +1,10 @@
+require('purecss/build/pure.css')
+
+require('font-awesome/css/font-awesome.css')
+
+require('./css/blackburn.css')
+require('./css/side-menu.css')
+require('./css/grids-responsive.css')
+require('./js/ui.js')
+
+require('./css/site.css')
