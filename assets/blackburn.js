fba956923937d78727f6885c7a2f9d4a89c530c3
William Hughes <williamh@catalyst.net.nz>
2016-09-22 09:45:15 +1200

    Update typography

--- assets/blackburn.js
+++ assets/blackburn.js
@@ -1,7 +1,8 @@
 require('purecss/build/pure.css')
 
 require('font-awesome/css/font-awesome.css')
-require('raleway-webfont/raleway.css')
+require('@typopro/web-raleway/TypoPRO-Raleway.css')
+require('@typopro/web-open-sans/TypoPRO-OpenSans.css')
 
 require('./css/blackburn.css')
 require('./css/side-menu.css')
